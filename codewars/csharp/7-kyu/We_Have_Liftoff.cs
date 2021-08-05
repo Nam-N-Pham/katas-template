e21eeb4338e598ba3f92b987113ccecfc9792a84
Nam Pham <phamnam3@hotmail.com>
2021-08-05 10:04:46 -0400

    Done

--- codewars/csharp/7-kyu/We_Have_Liftoff.cs
+++ codewars/csharp/7-kyu/We_Have_Liftoff.cs
@@ -1,10 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class Kata
 {
     public static string Liftoff(List<int> instructions)
     {
-        throw new NotImplementedException();
+        instructions.Sort();
+        instructions.Reverse();
+        var instructionsString = instructions.Select(instruction => instruction.ToString());
+        string countdown = "";
+        foreach (string instruction in instructionsString)
+        {
+            countdown = countdown + instruction + " ";
+        }
+        countdown = countdown + "liftoff!";
+        return countdown;
     }
 }
