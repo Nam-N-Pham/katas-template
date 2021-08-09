b9be2376344b8582f8cc965d450f4a17b925eaca
Nam Pham <phamnam3@hotmail.com>
2021-08-09 09:19:01 -0400

    Correct solution

--- codewars/csharp/7-kyu/Collatz_Conjecture_Length.cs
+++ codewars/csharp/7-kyu/Collatz_Conjecture_Length.cs
@@ -2,6 +2,23 @@ public class Kata
 {
     public static int Collatz(int n)
     {
-
+        int counter = 1;
+        if (n == 1)
+        {
+            return 4;
+        }
+        while (n > 1)
+        {
+            if (n % 2 == 0)
+            {
+                n = n / 2;
+            }
+            else
+            {
+                n = 3 * n + 1;
+            }
+            counter++;
+        }
+        return counter;
     }
 }
