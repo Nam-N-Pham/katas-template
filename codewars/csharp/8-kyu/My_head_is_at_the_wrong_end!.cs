b729e9807b963c9b7de59b65dad91dd6f4c16492
Nam Pham <phamnam3@hotmail.com>
2021-08-10 09:21:16 -0400

    Solved

--- codewars/csharp/8-kyu/My_head_is_at_the_wrong_end!.cs
+++ codewars/csharp/8-kyu/My_head_is_at_the_wrong_end!.cs
@@ -1,7 +1,12 @@
+using System.Linq;
+
 public class Kata
 {
     public static string[] FixTheMeerkat(string[] arr)
     {
-        //your code here
+        string temp = arr[0];
+        arr[0] = arr[2];
+        arr[2] = temp;
+        return arr;
     }
 }
