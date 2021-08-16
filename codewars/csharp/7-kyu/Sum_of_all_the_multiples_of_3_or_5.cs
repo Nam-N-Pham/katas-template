1803f509831f3906f38fdde5c7c8430b6d39a7f0
Nam Pham <phamnam3@hotmail.com>
2021-08-16 09:20:23 -0400

    Done

---
+++ codewars/csharp/7-kyu/Sum_of_all_the_multiples_of_3_or_5.cs
@@ -0,0 +1,21 @@
+namespace Solution
+{
+    public static class Program
+    {
+        public static int findSum(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % 3 == 0)
+                    sum += i;
+                if (i % 5 == 0)
+                    sum += i;
+                if (i % 3 == 0 && i % 5 == 0)
+                    sum -= i;
+            }
+
+            return sum;
+        }
+    }
+}
