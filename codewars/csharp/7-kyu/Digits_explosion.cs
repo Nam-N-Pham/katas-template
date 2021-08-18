a4212be47759b41e56bab4ac985d69bf1df1ead7
Nam Pham <phamnam3@hotmail.com>
2021-08-18 09:53:46 -0400

    Finished

--- codewars/csharp/7-kyu/Digits_explosion.cs
+++ codewars/csharp/7-kyu/Digits_explosion.cs
@@ -1,11 +1,26 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 public class Digits
 {
     public static string Explode(string s)
     {
-        // Insert your solution here
-        return "";
+        List<string> digits = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        string answer = "";
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int CurrentDigitAsInt = (int)Char.GetNumericValue(s[i]);
+            for (int j = CurrentDigitAsInt; j > 0; j--)
+            {
+                if (CurrentDigitAsInt == 0)
+                    continue;
+                answer += digits[CurrentDigitAsInt];
+            }
+
+        }
+
+        return answer;
     }
 }
