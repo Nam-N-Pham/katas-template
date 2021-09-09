eee3effcc29df20263a5486707f819104e0b4844
Nam Pham <phamnam3@hotmail.com>
2021-09-09 09:06:54 -0400

    Initial Commit

--- codewars/javascript/7-kyu/Counting_Array_Elements.js
+++ codewars/javascript/7-kyu/Counting_Array_Elements.js
@@ -1,3 +1,23 @@
 function count(array){
-  //your code here
+  let uniqueElements = {}
+  let count = 0
+
+  for (let index = 0; index < array.length; index++) 
+  {
+    if (!(array[index] in uniqueElements))
+    {
+      for (let j = index; j < array.length; j++) 
+      {
+        if (array[index] === array[j])
+        {
+          count++
+        }
+      }
+
+      uniqueElements.array[index] = count
+      count = 0
+    }
+  }
+
+  return uniqueElements
 }
