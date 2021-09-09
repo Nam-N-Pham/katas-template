2e7ecb79fba55a01b4374282cdf721ed8809c26d
Nam Pham <phamnam3@hotmail.com>
2021-09-09 16:10:08 -0400

    Done with Counting_Array_Elements

--- codewars/javascript/7-kyu/Counting_Array_Elements.js
+++ codewars/javascript/7-kyu/Counting_Array_Elements.js
@@ -1,23 +1,36 @@
-function count(array){
+function count(array) {
   let uniqueElements = {}
   let count = 0
 
-  for (let index = 0; index < array.length; index++) 
-  {
-    if (!(array[index] in uniqueElements))
-    {
-      for (let j = index; j < array.length; j++) 
-      {
-        if (array[index] === array[j])
-        {
+  for (let index = 0; index < array.length; index++) {
+    if (!(array[index] in uniqueElements)) {
+      for (let j = index; j < array.length; j++) {
+        if (array[index] === array[j]) {
           count++
         }
       }
 
-      uniqueElements.array[index] = count
+      uniqueElements[array[index]] = count
       count = 0
     }
   }
 
   return uniqueElements
+
+  // let uniqueElements = {}
+
+  // for (let index = 0; index < array.length; index++)
+  // {
+  //   if (!(array[index] in uniqueElements))
+  //   {
+  //     uniqueElements[array[index]] = 1
+
+  //   }
+  //   else
+  //   {
+  //     uniqueElements[array[index]]++
+  //   }
+  // }
+
+  // return uniqueElements
 }
