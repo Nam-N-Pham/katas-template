e1ea211bec787b9da836b806ac6531a41a3b8217
Nam Pham <phamnam3@hotmail.com>
2021-09-15 09:34:17 -0400

    Started Typescript Kyu 6 Find The Parity Outlier

--- codewars/javascript/6-kyu/Christmas_tree.js
+++ codewars/javascript/6-kyu/Christmas_tree.js
@@ -1,1 +1,21 @@
-function christmasTree(height) {}
+function christmasTree(height) {
+  if (height === 0) return ''
+
+  let answer = ''
+
+  const star = '*'
+  let currentLine = ''
+  for (let i = 1; i <= height; i++) {
+    currentLine = star.repeat(i * 2 - 1)
+    let maxCharacterOfLastLine = height * 2 - 1
+    let totalPaddingOfCurrentLine = maxCharacterOfLastLine - (i * 2 - 1)
+    currentLine = currentLine.padStart(
+      totalPaddingOfCurrentLine / 2 + (i * 2 - 1)
+    )
+    currentLine = currentLine.padEnd(maxCharacterOfLastLine)
+    answer = answer + currentLine
+    if (i < height) answer = answer + '\n'
+  }
+
+  return answer
+}
---
+++ codewars/typescript/6-kyu/Find_The_Parity_Outlier.ts
@@ -0,0 +1,22 @@
+export function findOutlier(integers: number[]): number {
+  let numOfEvens = 0
+  let numOfOdds = 0
+  let answer = 0
+
+  for (let i = 0; i < integers.length; i++) {
+    if (integers[i] % 2 === 0) numOfEvens++
+    if (integers[i] % 2 === 1) numOfOdds++
+  }
+  if (numOfOdds === 1) {
+    for (let i = 0; i < integers.length; i++) {
+      if (integers[i] % 2 === 1) answer = integers[i]
+    }
+  }
+  if (numOfEvens === 1) {
+    for (let i = 0; i < integers.length; i++) {
+      if (integers[i] % 2 === 0) answer = integers[i]
+    }
+  }
+
+  return answer
+}
