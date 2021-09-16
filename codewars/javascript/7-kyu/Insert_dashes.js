9858f70bcf17ea37c2ba59225ad55389ed4c567f
Nam Pham <phamnam3@hotmail.com>
2021-09-16 10:52:55 -0400

    Completed Javascript 7 Kyu Insert dashes

--- codewars/javascript/7-kyu/Insert_dashes.js
+++ codewars/javascript/7-kyu/Insert_dashes.js
@@ -1,3 +1,18 @@
 function insertDash(num) {
-  //code me
+  const numAsString = num.toString();
+  let answer = "";
+
+  for (let i = 0; i < numAsString.length - 1; i++) {
+    if (
+      Number(numAsString[i]) % 2 === 1 &&
+      Number(numAsString[i + 1] % 2 === 1)
+    ) {
+      answer = answer + numAsString[i] + "-";
+    } else {
+      answer = answer + numAsString[i];
+    }
+  }
+  answer = answer + numAsString[numAsString.length - 1];
+
+  return answer;
 }
