7d3fb5c185432e638eee9366b53a203ebd1cd2b3
Nam Pham <phamnam3@hotmail.com>
2021-09-30 22:33:03 -0400

    Passed Javascript 5 Kyu Where my anagrams at?

--- codewars/javascript/5-kyu/Where_my_anagrams_at.js
+++ codewars/javascript/5-kyu/Where_my_anagrams_at.js
@@ -1,1 +1,48 @@
-function anagrams(word, words) {}
+function anagrams(word, words) {
+  const alphabetValue = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+
+  let answer = [];
+
+  let valueOfWord = 0;
+  for (let i = 0; i < word.length; i++) {
+    valueOfWord += alphabetValue[word[i]];
+  }
+
+  let currentWordValue = 0;
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words[i].length; j++) {
+      currentWordValue += alphabetValue[words[i][j]];
+    }
+    if (currentWordValue === valueOfWord) answer.push(words[i]);
+    currentWordValue = 0;
+  }
+
+  return answer;
+}
