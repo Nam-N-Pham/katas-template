c02d0b237fe47ff9b2d0687b9fa1f0a3438550e7
Nam Pham <phamnam3@hotmail.com>
2021-10-04 09:16:35 -0400

    Passed JavaScript 6 kyu Sum of Digits/Digital Root

--- codewars/javascript/6-kyu/Sum_of_Digits/Digital_Root.js
+++ codewars/javascript/6-kyu/Sum_of_Digits/Digital_Root.js
@@ -1,3 +1,10 @@
 function digital_root(n) {
-  // ...
+  if (n < 10) return n;
+  let sum = 0;
+  while (n > 0) {
+    const currentDigit = n % 10;
+    sum += currentDigit;
+    n = Math.floor(n / 10);
+  }
+  return digital_root(sum);
 }
