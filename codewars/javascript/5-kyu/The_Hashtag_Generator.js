a5adb705a5535a051f8be7eefae5daa73109f80e
Nam Pham <phamnam3@hotmail.com>
2021-10-05 11:07:42 -0400

    Passed JavaScript 5 Kyu The Hashtag Generator

--- codewars/javascript/5-kyu/The_Hashtag_Generator.js
+++ codewars/javascript/5-kyu/The_Hashtag_Generator.js
@@ -1,1 +1,24 @@
-function generateHashtag(str) {}
+function generateHashtag(str) {
+  const trimmedStr = str.trim();
+
+  if (trimmedStr === "") return false;
+
+  const regex = /\s+/;
+  const arrayOfWords = trimmedStr.split(regex);
+
+  // if (arrayOfWords.length === 0) return false;
+
+  for (let i = 0; i < arrayOfWords.length; i++) {
+    let upperCaseFirstLetter = arrayOfWords[i][0].toUpperCase();
+    arrayOfWords[i] = upperCaseFirstLetter + arrayOfWords[i].substring(1);
+  }
+
+  let result = "#";
+  result += arrayOfWords.join("");
+
+  if (result.length <= 140) {
+    return result;
+  } else {
+    return false;
+  }
+}
