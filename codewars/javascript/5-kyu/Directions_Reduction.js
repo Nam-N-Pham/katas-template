f61b919e951c72177385e11af39f3641d539de30
Nam Pham <phamnam3@hotmail.com>
2021-10-07 22:08:59 -0400

    Passed JavaScript 5 Kyu Directions_Reduction

--- codewars/javascript/5-kyu/Directions_Reduction.js
+++ codewars/javascript/5-kyu/Directions_Reduction.js
@@ -1,3 +1,23 @@
 function dirReduc(arr) {
-  // ...
+  const reductionRule = {
+    NORTH: "SOUTH",
+    EAST: "WEST",
+    SOUTH: "NORTH",
+    WEST: "EAST",
+  };
+
+  let answer = [arr[0]];
+
+  for (let i = 1; i < arr.length; i++) {
+    answer.push(arr[i]);
+    while (
+      answer.length > 1 &&
+      reductionRule[answer[answer.length - 1]] === answer[answer.length - 2]
+    ) {
+      answer.pop();
+      answer.pop();
+    }
+  }
+
+  return answer;
 }
