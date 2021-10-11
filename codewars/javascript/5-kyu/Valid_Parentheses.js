97653e75b23841c5274c70e0aa4d9a14d09b7e76
Nam Pham <phamnam3@hotmail.com>
2021-10-11 10:32:17 -0400

    Passed JavaScript 5 kyu Valid Parentheses

--- codewars/javascript/5-kyu/Valid_Parentheses.js
+++ codewars/javascript/5-kyu/Valid_Parentheses.js
@@ -1,1 +1,10 @@
-function validParentheses(parens) {}
+function validParentheses(parens) {
+  let openParen = 0;
+
+  for (let i = 0; i < parens.length; i++) {
+    parens[i] === "(" ? openParen++ : openParen--;
+    if (openParen < 0) return false;
+  }
+
+  return openParen === 0 ? true : false;
+}
