0264f5c23a6e095007c87a36b134074d85d2f952
Nam Pham <phamnam3@hotmail.com>
2021-10-12 10:59:20 -0400

    Passed JavaScript 5 kyu Maximum subarray sum

--- codewars/javascript/5-kyu/Maximum_subarray_sum.js
+++ codewars/javascript/5-kyu/Maximum_subarray_sum.js
@@ -1,3 +1,25 @@
 var maxSequence = function (arr) {
-  // ...
+  if (arr === []) return 0;
+
+  let allNegative = true;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.length[i] > 0) allNegative = false;
+  }
+  if (!allNegative) return 0;
+
+  let maxSum = 0;
+  let currentSum = 0;
+  let currentArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      currentArr = arr.slice(i, j + 1);
+      for (let k = 0; k < currentArr.length; k++) {
+        currentSum += currentArr[k];
+        if (currentSum > maxSum) maxSum = currentSum;
+      }
+      currentSum = 0;
+    }
+  }
+
+  return maxSum;
 };
