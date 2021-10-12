965f13a8800c874dcc16f20efb5b8321437908da
Nam Pham <phamnam3@hotmail.com>
2021-10-12 11:47:02 -0400

    Passed JavaScript 5 kyu Simple Pig Latin

--- codewars/javascript/5-kyu/Simple_Pig_Latin.js
+++ codewars/javascript/5-kyu/Simple_Pig_Latin.js
@@ -1,3 +1,14 @@
 function pigIt(str) {
-  //Code here
+  let splitString = str.split(" ");
+
+  for (let i = 0; i < splitString.length; i++) {
+    if (!(splitString[i].search(/\w/g) === -1)) {
+      splitString[i] =
+        splitString[i].substring(1, splitString[i].length) +
+        splitString[i][0] +
+        "ay";
+    } else continue;
+  }
+
+  return splitString.join(" ");
 }
