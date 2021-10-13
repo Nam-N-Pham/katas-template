043f6d691fe0f19f73fdd37028d976bab0cc8056
Nam Pham <phamnam3@hotmail.com>
2021-10-13 19:35:36 -0400

    Passed JavaScript 5 kyu Moving Zeros To The End

--- codewars/javascript/5-kyu/Moving_Zeros_To_The_End.js
+++ codewars/javascript/5-kyu/Moving_Zeros_To_The_End.js
@@ -1,1 +1,10 @@
-var moveZeros = function (arr) {};
+var moveZeros = function (arr) {
+  let nonZeros = [];
+  let zeros = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] === 0 ? zeros.push(0) : nonZeros.push(arr[i]);
+  }
+
+  return nonZeros.concat(zeros);
+};
