7b7def2dd4df14d53a83f37e610734b263dc6321
Nam Pham <phamnam3@hotmail.com>
2021-10-13 20:02:12 -0400

    Passed JavaScript 5 kyu Human Readable Time

--- codewars/javascript/5-kyu/Human_Readable_Time.js
+++ codewars/javascript/5-kyu/Human_Readable_Time.js
@@ -1,1 +1,18 @@
-function humanReadable(seconds) {}
+function humanReadable(seconds) {
+  let secondsRemaining = seconds;
+
+  let hrs = Math.trunc(seconds / 3600);
+  secondsRemaining -= hrs * 3600;
+
+  let mins = Math.trunc(secondsRemaining / 60);
+
+  let secs = secondsRemaining - mins * 60;
+
+  return (
+    hrs.toString().padStart(2, "0") +
+    ":" +
+    mins.toString().padStart(2, "0") +
+    ":" +
+    secs.toString().padStart(2, "0")
+  );
+}
