d17e5e3030fbb137f8b206911d29239941439847
Nam Pham <phamnam3@hotmail.com>
2021-10-15 21:19:22 -0400

    Passed JavaScript 5 kyu Product of consecutive Fib numbers

--- codewars/javascript/5-kyu/Product_of_consecutive_Fib_numbers.js
+++ codewars/javascript/5-kyu/Product_of_consecutive_Fib_numbers.js
@@ -1,1 +1,16 @@
-function productFib(prod) {}
+function productFib(prod) {
+  let fnMinus2 = 0;
+  let fnMinus1 = 1;
+
+  while (fnMinus2 * fnMinus1 <= prod) {
+    let fn = fnMinus2 + fnMinus1;
+    fnMinus2 = fnMinus1;
+    fnMinus1 = fn;
+
+    if (fnMinus2 * fnMinus1 === prod) {
+      return [fnMinus2, fnMinus1, true];
+    }
+  }
+
+  return [fnMinus2, fnMinus1, false];
+}
