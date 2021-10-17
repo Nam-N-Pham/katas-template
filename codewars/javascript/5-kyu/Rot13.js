e23dec80063d9497c3bbdc4eead2308f2c8dfca4
Nam Pham <phamnam3@hotmail.com>
2021-10-16 22:13:32 -0400

    Passed JavaScript 5 kyu Rot13

--- codewars/javascript/5-kyu/Rot13.js
+++ codewars/javascript/5-kyu/Rot13.js
@@ -1,1 +1,15 @@
-function rot13(message) {}
+function rot13(message) {
+  const letters = message.split("");
+
+  let encryptedLetters = letters.map((letter) => {
+    if (letter.charCodeAt() >= 65 && letter.charCodeAt() <= 90) {
+      return String.fromCharCode(((letter.charCodeAt() - 65 + 13) % 26) + 65);
+    } else if (letter.charCodeAt() >= 97 && letter.charCodeAt() <= 122) {
+      return String.fromCharCode(((letter.charCodeAt() - 97 + 13) % 26) + 97);
+    } else {
+      return letter;
+    }
+  });
+
+  return encryptedLetters.join("");
+}
