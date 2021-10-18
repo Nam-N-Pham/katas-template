1f4688a663e4ffb1dd16dd6baf418157934cf661
Nam Pham <phamnam3@hotmail.com>
2021-10-18 11:13:12 -0400

    Passed JavaScript 5 kyu Best travel

--- codewars/javascript/5-kyu/Best_travel.js
+++ codewars/javascript/5-kyu/Best_travel.js
@@ -1,1 +1,30 @@
-function chooseBestSum(t, k, ls) {}
+function chooseBestSum(t, k, ls) {
+  if (k > ls.length) {
+    return null;
+  }
+
+  function choose(arr, k, prefix = []) {
+    if (k == 0) return [prefix];
+    return arr
+      .map((v, i) => choose(arr.slice(i + 1), k - 1, [...prefix, v]))
+      .reduce((acc, val) => acc.concat(val), []);
+  }
+
+  let groupsOfDistances = choose(ls, k);
+
+  let sum = 0;
+  let bestSum = 0;
+  for (let i = 0; i < groupsOfDistances.length; i++) {
+    for (let j = 0; j < groupsOfDistances[i].length; j++) {
+      sum += groupsOfDistances[i][j];
+    }
+
+    if (sum > bestSum && sum <= t) {
+      bestSum = sum;
+    }
+
+    sum = 0;
+  }
+
+  return bestSum > 0 ? bestSum : null;
+}
