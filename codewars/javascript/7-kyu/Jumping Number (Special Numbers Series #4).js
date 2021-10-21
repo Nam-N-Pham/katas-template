4e547079370fdba8094cf72bf2a12edd0428dfc1
Nam Pham <phamnam3@hotmail.com>
2021-10-20 21:47:41 -0400

    Passed JavaScript 7 kyu Jumping Numbers (Special Numbers Series #4)

--- codewars/javascript/7-kyu/Jumping Number (Special Numbers Series #4).js
+++ codewars/javascript/7-kyu/Jumping Number (Special Numbers Series #4).js
@@ -1,1 +1,39 @@
-function jumpingNumber(n) {}
+function jumpingNumber(n) {
+  const digits = n
+    .toString()
+    .split("")
+    .map((digit) => parseInt(digit));
+
+  if (digits.length === 1) return "Jumping!!";
+
+  for (let i = 0; i < digits.length; i++) {
+    if (i === 0) {
+      if (digits[i + 1] === digits[i] + 1 || digits[i + 1] === digits[i] - 1) {
+        continue;
+      } else {
+        return "Not!!";
+      }
+    }
+
+    if (i === digits.length - 1) {
+      if (digits[i - 1] === digits[i] + 1 || digits[i - 1] === digits[i] - 1) {
+        return "Jumping!!";
+      } else {
+        return "Not!!";
+      }
+    }
+
+    if (digits.length > 2) {
+      if (
+        (digits[i - 1] === digits[i] + 1 || digits[i - 1] === digits[i] - 1) &&
+        (digits[i + 1] === digits[i] + 1 || digits[i + 1] === digits[i] - 1)
+      ) {
+        continue;
+      } else {
+        return "Not!!";
+      }
+    }
+  }
+
+  return "Jumping!!";
+}
