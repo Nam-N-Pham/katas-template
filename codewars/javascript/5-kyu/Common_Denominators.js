a20b5ebd7cbffddc71dbd236e8aef1b762a12324
Nam Pham <phamnam3@hotmail.com>
2021-10-22 10:32:25 -0400

    Passed Common Denominators

--- codewars/javascript/5-kyu/Common_Denominators.js
+++ codewars/javascript/5-kyu/Common_Denominators.js
@@ -1,1 +1,34 @@
-function convertFrac(lst) {}
+function convertFrac(lst) {
+  if (lst.length === 0) {
+    return "";
+  }
+
+  const denoms = lst.map((fraction) => fraction[1]);
+  const highestDenom = Math.max(...denoms);
+
+  let commonDenom = highestDenom;
+  let foundCommonDenom = false;
+  while (!foundCommonDenom) {
+    for (let i = 0; i < denoms.length; i++) {
+      if (commonDenom % denoms[i] !== 0) {
+        commonDenom += highestDenom;
+        break;
+      }
+      if (
+        commonDenom % denoms[denoms.length - 1] === 0 &&
+        i === denoms.length - 1
+      ) {
+        foundCommonDenom = true;
+      }
+    }
+  }
+
+  const lstMultipliedByCommonDenom = lst.map((fraction) => [
+    fraction[0] * (commonDenom / fraction[1]),
+    commonDenom,
+  ]);
+
+  return lstMultipliedByCommonDenom
+    .map((fraction) => `(${fraction[0]},${fraction[1]})`)
+    .join("");
+}
