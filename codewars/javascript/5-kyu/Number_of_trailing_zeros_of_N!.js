53aa921a8969c685e2eec8c9d55f0e3c3641ef06
Nam Pham <phamnam3@hotmail.com>
2021-10-25 10:06:21 -0400

    Passed JavaScript 5 kyu Number of trailing zeroes of N!

--- codewars/javascript/5-kyu/Number_of_trailing_zeros_of_N!.js
+++ codewars/javascript/5-kyu/Number_of_trailing_zeros_of_N!.js
@@ -1,1 +1,13 @@
-function zeros(n) {}
+function zeros(n) {
+  function getBaseLog(x, y) {
+    return Math.log(y) / Math.log(x);
+  }
+
+  let trailingZeros = 0;
+
+  for (let i = 1; i <= Math.floor(getBaseLog(5, n)); i++) {
+    trailingZeros += Math.floor(n / Math.pow(5, i));
+  }
+
+  return trailingZeros;
+}
