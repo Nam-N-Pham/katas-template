377d4e2a95891da121d3cc28c9153804bffa6ae4
Nam Pham <phamnam3@hotmail.com>
2021-10-25 19:24:20 -0400

    Passed JavaScript 5 kyu Integers Recreation One

--- codewars/javascript/5-kyu/Integers_Recreation_One.js
+++ codewars/javascript/5-kyu/Integers_Recreation_One.js
@@ -1,1 +1,38 @@
-function listSquared(m, n) {}
+function listSquared(m, n) {
+  let answer = [];
+
+  for (let i = m; i <= n; i++) {
+    let lowerBound = 1;
+    let upperBound = i;
+    let divisors = [];
+
+    if (i === 1) {
+      answer.push([1, 1]);
+      continue;
+    }
+
+    while (lowerBound < upperBound) {
+      if (i % lowerBound === 0) {
+        divisors.push(lowerBound);
+        upperBound = i / lowerBound;
+        if (upperBound !== lowerBound) {
+          divisors.push(upperBound);
+        }
+      }
+
+      lowerBound++;
+    }
+
+    let sumOfSquaredDivisors = divisors
+      .map((divisor) => divisor ** 2)
+      .reduce((previousValue, currentValue) => {
+        return previousValue + currentValue;
+      }, 0);
+
+    if (sumOfSquaredDivisors ** 0.5 % 1 === 0) {
+      answer.push([i, sumOfSquaredDivisors]);
+    }
+  }
+
+  return answer;
+}
